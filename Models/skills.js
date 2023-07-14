d7cbb7d6b9da1c7adf0d101ec805fa1c610435c6
AlainL24 <lantigua.alain@gmail.com>
2023-07-13 22:51:17 -0400

    Create skills.js

---
+++ Models/skills.js
@@ -0,0 +1,30 @@
+const skills = [
+    {
+      id: 1,
+      name: 'JavaScript',
+      description: 'A programming language for the web',
+      level: 'Intermediate',
+    },
+    {
+      id: 2,
+      name: 'HTML',
+      description: 'A markup language for creating web pages',
+      level: 'Beginner',
+    },
+    {
+      id: 3,
+      name: 'CSS',
+      description: 'A style sheet language used for designing web pages',
+      level: 'Intermediate',
+    },
+    // Add more skills here
+  ];
+  
+  module.exports = {
+    getAllSkills: () => {
+      return skills;
+    },
+    getSkillById: (id) => {
+      return skills.find((skill) => skill.id === id);
+    },
+  };
