5926003df14176e56d5fc872c7b7520dc28009c5
AlainL24 <lantigua.alain@gmail.com>
2023-07-14 00:16:32 -0400

    Update skills.js

--- Controllers/skills.js
+++ Controllers/skills.js
@@ -1,21 +1,43 @@
 const skillModel = require('../models/skill');
 
-
 module.exports = {
-    index: (req, res) => {
+  index: (req, res) => {
+    const skills = skillModel.getAllSkills();
+    res.render('skills/index', { skills });
+  },
+  new: (req, res) => {
+    res.render('skills/new');
+  },
+  create: (req, res) => {
+    const { name, description, level } = req.body;
+    const newSkill = {
+      id: skillModel.getAllSkills().length + 1,
+      name,
+      description,
+      level,
+    };
+    skillModel.getAllSkills().push(newSkill);
+    res.redirect('/skills');
+  },
+  show: (req, res) => {
+    const skillId = parseInt(req.params.id);
+    const skill = skillModel.getSkillById(skillId);
 
-        const skills = skillModel.getAllSkills();
-        res.render('skills/index', { skills });
-      },
+    if (skill) {
+      res.render('skills/show', { skill });
+    } else {
+      res.status(404).send('Skill not found');
+    }
+  },
+  delete: (req, res) => {
+    const skillId = parseInt(req.params.id);
+    const skills = skillModel.getAllSkills();
+    const index = skills.findIndex((skill) => skill.id === skillId);
 
-      show: (req, res) => {
-        const skillId = parseInt(req.params.id);
-        const skill = skillModel.getSkillById(skillId);
+    if (index !== -1) {
+      skills.splice(index, 1);
+    }
 
-        if (skill) {
-            res.render('skills/show', { skill });
-          } else {
-            res.status(404).send('Skill not found');
-          }
-        },
-      };
+    res.redirect('/skills');
+  },
+};
