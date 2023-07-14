8b09381de348e37436c46f4cce6ac2c2d2ef77e1
AlainL24 <lantigua.alain@gmail.com>
2023-07-14 00:16:34 -0400

    Update skills.js

--- Routes/skills.js
+++ Routes/skills.js
@@ -5,7 +5,11 @@ const router = express.Router();
 const skillsController = require('../controllers/skills');
 
 router.get('/', skillsController.index);
+router.get('/new', skillsController.new);
+router.post('/', skillsController.create);
 
 router.get('/:id', skillsController.show);
+router.post('/:id/delete', skillsController.delete);
+
 
 module.exports = router;
